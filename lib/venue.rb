b1a1abaea695163d15506021c795cb1ae64b8ac7
deebot10 <dee1jhill@gmail.com>
2021-03-24 15:59:20 -0600

    Challenge

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,29 @@
 class Venue
+  attr_accessor :name,
+                :capacity,
+                :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(customer)
+    @patrons.push(customer)
+  end
+
+  # def yell_at_patrons
+  #
+  #   return @patrons.upcase
+  #
+  # end
+
+  def over_capacity?
+     return false if @patrons.length == 3
+     true
+  end
+
+  def kick_out
+  end 
 end
